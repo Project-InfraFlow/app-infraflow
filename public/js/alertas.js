0592f298abedd2e55ea2713bdaf1f15f47b17827
nicollyterra <nicolly.terra@sptech.school>
2025-11-21 15:43:07 -0300

    style(dashboard alertas): Front-end da tela de alertas

---
+++ public/js/alertas.js
@@ -0,0 +1,20 @@
+ const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['CPU', 'RAM', 'Disco', 'Rede'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
