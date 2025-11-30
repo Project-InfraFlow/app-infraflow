9383ef844b11d3f6c2694c2132c77dbcc24caa0b
julia <julia.gavioli@sptech.school>
2025-11-29 23:27:14 -0300

    Subindo alteração no cpuModel.js

--- src/models/cpuModel.js
+++ src/models/cpuModel.js
@@ -7,13 +7,16 @@ function buscarDados(maquinaId, limite) {
             l.data_hora_captura AS horario,
 
             MAX(CASE WHEN c.nome_componente = 'CPU' THEN l.dados_float END) AS cpu,
-            MAX(CASE WHEN c.nome_componente = 'Latência CPU' THEN l.dados_float END) AS latencia,
+            MAX(CASE WHEN c.nome_componente = 'CPU Idle' THEN l.dados_float END) AS latencia,
             MAX(CASE WHEN c.nome_componente = 'Processos' THEN l.dados_float END) AS processos
 
         FROM leitura l
         JOIN componente c 
             ON c.id_componente = l.fk_id_componente
+
         WHERE l.fk_id_maquina = ${maquinaId}
+          AND c.nome_componente IN ('CPU','CPU Idle','Processos')
+
         GROUP BY l.data_hora_captura
         ORDER BY l.data_hora_captura DESC
         LIMIT ${limite};
